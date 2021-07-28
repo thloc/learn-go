dabf8855245ffc36fdc3757a76b75e7560a60cb4
THLoc <mail.tranhuuloc@gmail.com>
2021-07-28 22:00:40 +0700

    Global and block scope

--- 2_variable.go
+++ 2_variable.go
@@ -2,32 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
-	var x float64
-	x = 20.0
-	fmt.Println(x)
-	fmt.Printf("x is of type %T\n", x)
+// func main() {
+// 	var x float64
+// 	x = 20.0
+// 	fmt.Println(x)
+// 	fmt.Printf("x is of type %T\n", x)
 
-	fmt.Println("----------------------")
-	var a float64 = 20.0
-	b := 42
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Printf("x is of type %T\n", a)
-	fmt.Printf("y is of type %T\n", b)
+// 	fmt.Println("----------------------")
+// 	var a float64 = 20.0
+// 	b := 42
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+// 	fmt.Printf("x is of type %T\n", a)
+// 	fmt.Printf("y is of type %T\n", b)
 
-	fmt.Println("----------------------")
-	k := 5
-	fmt.Printf("k is of type %T\n", k)
+// 	fmt.Println("----------------------")
+// 	k := 5
+// 	fmt.Printf("k is of type %T\n", k)
 
-	fmt.Println("----------------------")
-	const Pi = 3.14 // const Pi := 3.14 ---> sai (:=)
+// 	fmt.Println("----------------------")
+// 	const Pi = 3.14 // const Pi := 3.14 ---> sai (:=)
 
-	fmt.Println("----------------------")
-	Big = 1 << 100 // mang 1 dich qua trai 100 bit
-	// 1 << 2 = 100 (bit): tang them 00 la gia tri tang gap doi
-	// 4 >> 2 = 0 (bit)
-}
+// 	fmt.Println("----------------------")
+// 	Big = 1 << 100 // mang 1 dich qua trai 100 bit
+// 	// 1 << 2 = 100 (bit): tang them 00 la gia tri tang gap doi
+// 	// 4 >> 2 = 0 (bit)
+// }
 
 // Golang ko có public, private & static. Nếu muốn export phải viết Hoa
 /* Kieu du lieu
@@ -39,3 +39,17 @@ func main() {
  * rune // alias for int32 -> Dung trong lap ky tu trong chuoi
  * float32 float64
  */
+
+//  ------- Global and block scope ------------
+
+var (
+	n int    = 10
+	m int    = 20
+	h string = "hello"
+)
+
+func main() {
+	fmt.Println(n)
+	fmt.Println(m)
+	fmt.Println(h)
+}
