54f07282c235e7f6701522108054960a5dee08a2
THLoc <mail.tranhuuloc@gmail.com>
2021-07-29 11:37:07 +0700

    WIP Interface

--- 9_interface.go
+++ 9_interface.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	// "math"
 )
@@ -43,6 +44,7 @@ import (
 // }
 
 // ---------------------------------
+// ------ thong qua struct ---------
 type I interface {
 	M()
 }
@@ -59,4 +61,91 @@ func (t T) M() {
 func main() {
 	var i I = T{"hello"}
 	i.M()
+
+	fmt.Println("---------- ko thong qua struct ------------")
+	myInt := IntCounter(0)
+	var inc Incrementer = &myInt
+	for i := 1; i < 5; i++ {
+		fmt.Println(inc.Increment())
+	}
+
+	fmt.Println("---------- Nhieu interface ------------")
+	var wc WriterCloser = NewBufferedWriterCloser()
+	wc.Write([]byte("Hello world"))
+	wc.Close()
+}
+
+// -------------------------------
+// --- ko thong qua struct ----
+type Incrementer interface {
+	Increment() int
+}
+
+type IntCounter int
+
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
+
+// ------------- Nhieu interface ------------------
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type Closer interface {
+	Close() error
+}
+
+type WriterCloser interface {
+	Writer
+	Closer
+}
+
+type BufferedWriterCloser struct {
+	buffer *bytes.Buffer
+}
+
+func (bwc *BufferedWriterCloser) Write(data []byte) (int, error) {
+	n, err := bwc.buffer.Write(data)
+
+	if err != nil {
+		return 0, err
+	}
+
+	v := make([]byte, 8)
+
+	for bwc.buffer.Len() > 8 {
+		_, err := bwc.buffer.Read(v)
+
+		if err != nil {
+			return 0, err
+		}
+
+		_, err = fmt.Println(string(v))
+
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return n, nil
+}
+
+func (bwc *BufferedWriterCloser) Close() error {
+	for bwc.buffer.Len() > 0 {
+		data := bwc.buffer.Next(8)
+		_, err := fmt.Println(string(data))
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func NewBufferedWriterCloser() *BufferedWriterCloser {
+	return &BufferedWriterCloser{
+		buffer: bytes.NewBuffer([]byte{}),
+	}
 }
