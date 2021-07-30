79ca674cd20976775af21dd68b2b65efdf5dbe17
THLoc <mail.tranhuuloc@gmail.com>
2021-07-30 08:33:18 +0700

    Fix todo file

--- learn/todo.go
+++ learn/todo.go
@@ -9,30 +9,30 @@ import (
 func main() {
 	limit := 5
 	listURL := make(chan int, 200)
-	results := make(chan int, limit)
+	results := make(chan int, 200)
+	defer close(listURL)
 
 	for i := 0; i < 200; i++ {
 		listURL <- i
 	}
-	results2 := crawlData(listURL, results, limit)
 
-	for c := range results2 {
+	go crawlData(listURL, results, limit)
+
+	for c := range results {
 		fmt.Println("read value", c, "from results")
 	}
 	fmt.Println("Crawl finished!")
 }
 
-func crawlData(listURL chan int, results chan int, limit int) <-chan int {
+func crawlData(listURL chan int, results chan int, limit int) {
+	defer close(results)
+
 	if limit > len(listURL) {
 		limit = len(listURL)
 	}
 
 	for i := 1; i <= limit; i++ {
-		go func() {
-			results <- <-listURL
-			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Microsecond)
-		}()
+		results <- <-listURL
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Microsecond)
 	}
-	close(listURL)
-	return results
 }
